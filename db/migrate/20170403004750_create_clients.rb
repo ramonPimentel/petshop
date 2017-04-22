42b2edd7350152746a0a56ad3d80c7284ec9c29a
Ramon Pimentel <ramonpimentel92@gmail.com>
2017-04-21 23:55:08 -0300

    Acerta migrate

--- db/migrate/20170403004750_create_clients.rb
+++ db/migrate/20170403004750_create_clients.rb
@@ -2,8 +2,8 @@ class CreateClients < ActiveRecord::Migration[5.0]
   def change
     create_table :clients do |t|
       t.string :name
-      t.integer :phone, :limit => 11
-      t.integer :cellphone, :limit => 11
+      t.integer :phone
+      t.integer :cellphone
       t.string :description
 
       t.timestamps
